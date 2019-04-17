7ec831908c81dc6a8b147158e576276b5955f6ac
yanis <yanis@octopouce.mu>
2019-04-17 12:38:03 +0400

    support : time tracking on frontend and event binding of timelineLite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,28 @@
-import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import {TimelineLite, Linear} from 'gsap';
+import {AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, ViewChild} from '@angular/core';
+import {Linear, TimelineLite} from 'gsap';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent implements AfterViewInit {
 
   @ViewChild('progress') progressElement: ElementRef;
 
   public timeline = new TimelineLite();
-
+  public time = 0;
+  constructor(private changeRef: ChangeDetectorRef) { }
   ngAfterViewInit() {
     this.start();
   }
 
   start() {
+    this.timeline.eventCallback('onUpdate', () => {
+      this.time = this.timeline.time();
+      this.changeRef.detectChanges();
+    }, [] );
     this.timeline.fromTo(this.progressElement.nativeElement, 10, {width: '100%'}, {width: 0, ease: Linear.easeNone});
   }
 }
